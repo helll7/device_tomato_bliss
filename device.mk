d4d5236db97082cbf7567b8bfa83086d5950af2a
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2016-05-27 20:51:50 +0530

    tomato: Add Doze flags Reference: https://github.com/neobuddy89/android_device_yu_lettuce/blob/b9dc54b7d50667cd1d35228633ca2b8fdf038052/overlay/frameworks/base/core/res/res/values/config.xml

--- device.mk
+++ device.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Build ID
+TARGET_UNOFFICIAL_BUILD_ID := darshan1205
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     yl_btmac
