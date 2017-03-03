b82ae164d67c0f95481cf78e0707129004eb845b
Ritesh Saxena <riteshsax007@gmail.com>
2017-03-03 14:47:44 +0000

    Some Fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,3 +22,5 @@ TARGET_BOARD_INFO_FILE := device/yu/tomato/board-info.txt
 
 # Inherit from proprietary files
 -include vendor/yu/tomato/BoardConfigVendor.mk
+
+TARGET_SYSTEM_PROP += device/yu/tomato/system.prop
